31b6221dcdd628133baaca85c64166df27a02b5b
John DeLeon <makingcodeio@gmail.com>
2025-04-02 20:35:02 -0500

    initial cdk commit

---
+++ backend/app/main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from mangum import Mangum
+import os
+from dotenv import load_dotenv
+import uvicorn
+
+# Load environment variables from .env file
+load_dotenv()
+
+
+app = FastAPI()
+
+
+
+
+# Allow CORS for all origins (you can restrict this to specific origins if needed)
+allowed = os.getenv("ALLOWED_ORIGINS", "*")
+if allowed == "*":
+    allowed = ["*"]
+else:
+    allowed = [origin.strip() for origin in allowed.split(',')]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[allowed],
+    allow_credentials=True,
+    allow_methods=['get', 'post', 'options'],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+# This is the entry point for the AWS Lambda function.
+handler = Mangum(app)
+
+# The following code is for local testing. It will not be executed in AWS Lambda.
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', port=5000)
---
+++ cdk/cdk/cdk_stack.py
@@ -0,0 +1,55 @@
+from aws_cdk import (
+    # Duration,
+    Stack,
+    aws_s3 as s3,
+    aws_s3_deployment as s3deploy,
+    aws_cloudfront as cloudfront,
+    aws_cloudfront_origins as cloudfront_origins,
+    aws_lambda as _lambda,
+    aws_apigateway as apigateway,
+    Duration
+    # aws_sqs as sqs,
+)
+import os
+from constructs import Construct
+
+class CdkStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # Frontend
+        bucket = s3.Bucket(self, "MovieReviewSentimentAnalysisFrontend",
+                           website_index_document="index.html",
+                           bucket_name= os.getenv('FE_BUCKET_NAME'),
+                           public_read_access=False,
+                           removal_policy=s3.RemovalPolicy.DESTROY,
+                           auto_delete_objects=True)
+        origin_access_id = cloudfront.OriginAccessIdentity(self, "MovieReviewSentimentAnalysisOriginAccessIdentity")
+        bucket.grant_read(origin_access_id)
+
+        distribution = cloudfront.Distribution(self, "MovieReviewSentimentAnalysisDistribution",
+                                               default_root_object="index.html",
+                                               default_behavior=cloudfront.BehaviorOptions(
+                                                   origin=cloudfront_origins.S3Origin(bucket, origin_access_identity=origin_access_id),
+                                                   viewer_protocol_policy=cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS)
+
+                                               )
+        
+
+        # Backend 
+        docker_lambda = _lambda.DockerImageFunction(self, "MovieReviewSentimentAnalysisLambda",
+                                                    code=_lambda.DockerImageCode.from_image_asset(os.path.join('..', 'backend')),
+                                                    timeout=Duration.seconds(30)
+                                                    )
+        
+        # API Gateway
+        api = apigateway.LambdaRestApi(self, "MovieReviewSentimentAnalysisAPI",
+                                       handler=docker_lambda,
+                                       proxy=True,
+                                       description="Movie Review Sentiment Analysis API"
+                                       )
+        
+        self.api_url = api.url
+        self.bucket_name = bucket.bucket_name
+        self.distribution_id = distribution.distribution_id
